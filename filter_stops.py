ba516f8ea0b85c37575136cd7b3d5cc2859e37eb
Arkadiusz Klein <arek.klein@gmail.com>
2018-07-21 09:07:56 +0200

    10MB > 300kb stops filtering

--- filter_stops.py
+++ filter_stops.py
@@ -1,11 +1,24 @@
 import json
 
-with open('downloaded.json') as f:
+useful_keys = ['stopId', 'stopShortName', 'stopDesc', 'stopName', 'stopLat', 'stopLon']
+
+with open('downloaded.json', encoding="utf8") as f:
     data = json.load(f)
     data = data[list(data.keys())[0]]
     stops = {}
     for stop in data['stops']:
-        print(stop)
-        stops[stop.get('stopId')] = stop
-    with open('input.json', 'w') as output:
-        json.dump(stops, output)
+        stop_id = stop.get('stopId')
+        if stops.get(stop_id):
+            continue
+        new_stop = {}
+        for key, value in stop.items():
+            if key in useful_keys:
+                if key == 'stopId':
+                    key = 'id'
+                new_stop[key] = value
+        print(new_stop)
+        stops[stop_id] = new_stop
+    should_save = input('Do you want to save? (y/n)\n') == 'y'
+    if should_save:
+        with open('stops.json', 'w') as output:
+            json.dump(stops, output)
