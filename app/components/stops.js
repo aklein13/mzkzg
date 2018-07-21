e547a3a9e265e9c8757bec6291214137236f8b51
Arkadiusz Klein <arek.klein@gmail.com>
2018-07-21 23:01:43 +0200

    stop search, better render efficiency

--- app/components/stops.js
+++ app/components/stops.js
@@ -4,9 +4,40 @@ import {
   View,
   Text,
   FlatList,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
 } from 'react-native';
 
 const stopList = require('../../stops.json');
+
+const styles = StyleSheet.create({
+  stop: {
+    color: 'black',
+    fontSize: 18,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 5,
+  }
+});
+
+class Stop extends React.PureComponent {
+  handlePress = () => this.props.onItemPress(this.props.name);
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.handlePress}>
+        <View>
+          <Text style={styles.stop}>
+            {this.props.name}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 
 class Stops extends Component {
   constructor(props) {
@@ -17,14 +48,38 @@ class Stops extends Component {
     };
   }
 
+  handleSearchChange = (text) => {
+    let newStops = Object.keys(stopList);
+    if (text) {
+      const searchText = text.toLowerCase();
+      newStops = newStops.filter((stop) => stop.toLowerCase().includes(searchText));
+    }
+    this.setState({stops: newStops, search: text});
+  };
+
+  keyExtractor = (item) => item;
+
+  onStopPress = (name) => {
+    console.log(name);
+  };
+
+  renderStop = ({item}) => (
+    <Stop onItemPress={this.onStopPress} name={item}/>
+  );
+
   render() {
-    console.log(this.state.stops);
     return (
       <View>
-        <Text>Jello</Text>
+        <TextInput
+          style={{height: 50, marginLeft: 10, marginRight: 10, fontSize: 20}}
+          onChangeText={this.handleSearchChange}
+          value={this.state.search}
+          placeholder={'Wyszukaj'}
+        />
         <FlatList
           data={this.state.stops}
-          renderItem={({item}) => <Text key={item}>{item}</Text>}
+          renderItem={this.renderStop}
+          keyExtractor={this.keyExtractor}
         />
       </View>
     )
